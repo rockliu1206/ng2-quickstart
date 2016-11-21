9ca3038821f69fcfc6702fa88c8b1327bef67892
rockliu1206 <liuxy1206@126.com>
2016-11-21 19:56:25 +0800

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector : 'my-app',
-	templateUrl : 'app.component.html',
-	styleUrls : ['app.component.css']
+	template : `
+	app.component.html
+	`,
+	styles : [`
+	body{}
+	`]
 })
 
 export class AppComponent {}
